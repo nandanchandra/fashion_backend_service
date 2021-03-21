aa67b92f7da81ebd2eefcccc44a238fb2b129f17
implicitdefcncdragneel <chandranandan.chandrakar@gmail.com>
2021-03-21 15:45:45 +0530

    update 0

--- ecom/api/category/models.py
+++ ecom/api/category/models.py
@@ -7,3 +7,5 @@ class Category(models.Model):
     description =models.CharField(max_length=250)
     created_at =models.DateTimeField(auto_now_add=True)
     updated_at=models.DateTimeField(auto_now=True)
+
+    
