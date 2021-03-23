5bee92f64331ef153db2b7f3c750800adb066ca4
implicitdefcncdragneel <chandranandan.chandrakar@gmail.com>
2021-03-23 20:50:19 +0530

    Update serializers.py

--- ecom/api/user/serializers.py
+++ ecom/api/user/serializers.py
@@ -8,13 +8,23 @@ from .models import CustomeUser
 class UserSerializer(serializers.HyperlinkedModelSerializer):
 
     def create(self, validated_data):
-        password= validated_data.pop('')
+        password= validated_data.pop('password',None)
+        instance=self.Meta.model(**validated_data)
+
+        if password is not None:
+            instance.set_password(password)
+        instance.save()
+        return instance
        
 
     def update(self, instance, validated_data):
-        instance.email = validated_data.get('email', instance.email)
-        instance.content = validated_data.get('content', instance.content)
-        instance.created = validated_data.get('created', instance.created)
+        for attr,value in validated_data.items():
+            if attr == 'password':
+                instance.set_password(value)
+            else:
+                setattr(instance,attr,value)
+
+                
         instance.save()
         return instance
 
