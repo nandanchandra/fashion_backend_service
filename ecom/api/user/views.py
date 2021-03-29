e8854bffc8053fef1bb4e8f5f0c084271ec74124
implicitdefcncdragneel <chandranandan.chandrakar@gmail.com>
2021-03-30 00:36:06 +0530

    Update views.py

--- ecom/api/user/views.py
+++ ecom/api/user/views.py
@@ -9,8 +9,15 @@ from django.contrib.auth import login,logout
 import re
 import random
 
+
+
+
 def generate_session_token(length=10):
     return ''.join(random.SystemRandom().choice([chr(i) for i in range(97,123)] + [str(i) for i in range(10)]) for _ in range(10))
+
+
+
+# SIGN IN
 
 @csrf_exempt
 def signin(request):
@@ -53,3 +60,39 @@ def signin(request):
 
     except UserModel.DoesNotExist:
         return JsonResponse({'error':'Invalid Email'})
+
+
+
+# SIGN OUT
+
+def signout(request,id):
+    logout(request)
+
+    UserModel=get_user_model()
+
+    try:
+        user=UserModel.objects.get(pk=id)
+        user.session_token="0"
+        user.save()
+
+    except UserModel.DoesNotExist:
+        return JsonResponse({'error':'Invalid user ID'})
+
+    return JsonResponse({'success':'Logout Success'})
+
+
+
+# Standarizations
+
+class UserViewSet(viewsets.ModelViewSet):
+    permission_classes_by_action={'create':[AllowAny]}
+
+    queryset=CustomeUser.objects.all().order_by('id')
+    serializer_class= UserSerializer
+
+    def get_permission(self):
+        try:
+            return [permission() for permission in self.permission_classes_by_action[self.action]]
+            
+        except KeyError :
+            return [permission() for permission in self.permission_classes]
