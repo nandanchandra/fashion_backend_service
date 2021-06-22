c8b66957cee89e8a3d9bb71e0ae1b20e1a320436
implicitdefcncdragneel <chandranandan.chandrakar@gmail.com>
2021-06-22 21:43:37 +0530

    userModel

--- ecom/settings.py
+++ ecom/settings.py
@@ -43,7 +43,9 @@ INSTALLED_APPS = [
     'rest_framework.authtoken',
     
     'api',
-    'api.category'
+    'api.category',
+    'api.product',
+    'api.user'
 ]
 
 MIDDLEWARE = [
@@ -131,7 +133,7 @@ STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT =os.path.join(BASE_DIR,'media')
 
-# AUTH_USER_MODEL="user.CustomUser"
+AUTH_USER_MODEL="user.CustomUser"
 
 CORS_ORIGIN_ALLOW_ALL = True
 
