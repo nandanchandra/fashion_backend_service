f6e7568d12256b9773170dc7da1b6bdd040f1c54
Chandra Nandan <chandranandan.chandrakar@gmail.com>
2021-08-04 16:29:09 +0530

    paymentView

--- api/payment/views.py
+++ api/payment/views.py
@@ -18,3 +18,47 @@ gateway = braintree.BraintreeGateway(
     )
 )
 
+
+def validate_user_session(id, token):
+    UserModel = get_user_model()
+
+    try:
+        user = UserModel.objects.get(pk=id)
+        if user.session_token == token:
+            return True
+        return False
+    except UserModel.DoesNotExist:
+        return False
+
+# TODO: In return response insert the customer_id received from client
+@csrf_exempt
+def generate_token(request, id, token):
+    if not validate_user_session(id, token):
+        return JsonResponse({'error': 'Invalid session, Please login again!'})
+
+    return JsonResponse({'clientToken': gateway.client_token.generate(), 'success': True})
+
+
+@csrf_exempt
+def process_payment(request, id, token):
+    if not validate_user_session(id, token):
+        return JsonResponse({'error': 'Invalid session, Please login again!'})
+
+    nonce_from_the_client = request.POST["paymentMethodNonce"]
+    amount_from_the_client = request.POST["amount"]
+
+    result = gateway.transaction.sale({
+        "amount": amount_from_the_client,
+        "payment_method_nonce": nonce_from_the_client,
+        "options": {
+            "submit_for_settlement": True
+        }
+    })
+
+    if result.is_success:
+        return JsonResponse({
+            "success": result.is_success, 'transaction': {'id': result.transaction.id, 'amount': result.transaction.amount}})
+    else:
+        return JsonResponse({'error': True, 'sucess': False})
+
+
