76fa995cc68b6958d900173f2be1d6927b868431
Chandra Nandan <chandranandan.chandrakar@gmail.com>
2022-05-12 21:41:57 +0530

    v1

--- ecom/settings.py
+++ ecom/settings.py
@@ -12,15 +12,14 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 import os
 from pathlib import Path
 
-import json
+# import json
 
-with open('/home/cncdragneel/githubCnc/fashion_ecom_django_react/config.json') as config_file:
-    config = json.load(config_file)
+# with open('./config.json') as config_file:
+#     config = json.load(config_file)
 
-
-merchant_id = config['merchant_id']
-public_key = config['public_key']
-private_key = config['private_key']
+merchant_id = "sdffa"
+public_key = "asfewgd"
+private_key = "trhwtrhfdgs"
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
