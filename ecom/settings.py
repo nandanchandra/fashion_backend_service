b23200d7d33ea90ff9bad7d20d0ff2e4315c590d
Chandra Nandan <chandranandan.chandrakar@gmail.com>
2022-10-09 23:53:31 +0530

    DEBUG=False

--- ecom/settings.py
+++ ecom/settings.py
@@ -12,7 +12,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-=&-k&*9ha^*vzoy^09wm5e+d483i-)3nsm6iac8apb_0g!au8+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
